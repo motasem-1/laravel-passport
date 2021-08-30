272bfb9008a6a4eb3d5e8040c6ac92082325a47f
motasem-zyood <zyoud1133@gmail.com>
2021-08-30 18:30:48 +0300

    add api restfull project

---
+++ app/Http/Controllers/PassportController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+
+class PassportController extends Controller
+{
+    // register by api
+    public function register(Request $request)
+    {
+        /// check validation
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|min:3',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6',
+        ]);
+        /// if validation failed
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        /// if validation success create new user
+        $users = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => bcrypt($request->password),
+
+
+        ]);
+        /// generate new token
+        $token = $users->createToken('ZyoodAcademe')->accessToken;
+
+        return response()->json(['token' => $token], 200);
+    } // end of register function
+
+
+    // login user by api
+    public function login(Request $request)
+    {
+
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
+        /// check if user is exists in database
+        if (auth()->attempt($credentials)) {
+            $token = auth()->user()->createToken('ZyoodAcademe')->accessToken;
+
+            return response()->json(['token' => $token], 200);
+        } else {
+            return response()->json(['error' => 'unSAuthorized', 401]);
+        }
+    }
+
+    public function details()
+    {
+
+        return response()->json(['user' => auth()->user()], 200);
+    }
+}
---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class ProductController extends Controller
+{
+
+    public function __construct()
+    {
+        $this->middleware('api');
+    }
+    public function index()
+    {
+
+        $products = auth()->user()->products;
+
+        return response()->json([
+            'success' => true,
+            'data' => $products,
+
+        ]);
+    } // end of index fun...
+
+    public function show($id)
+    {
+        $products = auth()->user()->products()->find($id);
+
+        if (!$products) {
+            return response()->json([
+                'success' => false,
+                'data' => 'no data found for this id =' . $id,
+
+            ]);
+        }
+        return response()->json([
+
+            'success' => true,
+            'data' => $products->toArray(),
+        ], 200);
+    } // end of show fun...
+
+    public function store(Request $request)
+    {
+
+        /// check validation
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|min:3',
+            'price' => 'required|integer',
+
+        ]);
+        /// if validation failed
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+
+        $product = new Product();
+
+        $product->name = $request->name;
+        $product->price = $request->price;
+        if (auth()->user()->products()->save($product)) {
+
+
+            return response()->json([
+
+                'success' => true,
+                'data' => $product->toArray(),
+            ], 200);
+        } else {
+
+            return response()->json([
+
+                'success' => false,
+                'data' => 'Oops, product store Failed',
+            ], 422);
+        }
+    } // end of store fun...
+
+
+
+    public function update(Request $request, $id)
+    {
+        $product = auth()->user()->products()->find($id);
+
+        if (!$product) {
+            return response()->json([
+                'success' => false,
+                'data' => 'Product with id' . $id . 'not found',
+            ], 500);
+        }
+
+        $updated = $product->fill($request->all())->save();
+
+        if ($updated) {
+            return response()->json([
+                'success' => true,
+                'message' => 'Product updated successfully',
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Product could not be updated',
+            ], 500);
+        }
+    } // end of update fun ...
+
+
+    public function destroy($id)
+    {
+        $product = auth()->user()->products()->find($id);
+
+        if (!$product) {
+            return response()->json([
+                'success' => false,
+                'message' => 'Product with id' . $id . 'not found'
+            ], 400);
+        }
+
+        if ($product->delete()) {
+            return response()->json([
+                'success' => true,
+                'message' => "Product deleted successfully"
+            ]);
+        } else {
+            return response()->json([
+                'success' => false,
+                'message' => 'Product could not be deleted'
+            ], 500);
+        }
+    }
+}
